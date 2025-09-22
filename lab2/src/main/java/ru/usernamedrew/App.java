b175839c86d99a5f109e9baf6ee30d5efb80ab1a
usernamedrew <username.drew@yandex.ru>
2025-09-22 16:57:13 +0300

    Initial commit

---
+++ lab2/src/main/java/ru/usernamedrew/App.java
@@ -0,0 +1,13 @@
+package ru.usernamedrew;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
