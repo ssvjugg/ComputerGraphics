7ef77dcc03049763b2fa7995a04bbd6209b48a26
usernamedrew <username.drew@yandex.ru>
2025-09-22 17:17:12 +0300

    Added files for each task

--- lab2/src/main/java/ru/usernamedrew/App.java
+++ lab2/src/main/java/ru/usernamedrew/App.java
@@ -1,13 +1,34 @@
 package ru.usernamedrew;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class App {
+    public static void main( String[] args ) {
+        try {
+            File file = new File("./src/main/resources/images/star.png");
+            BufferedImage image = ImageIO.read(file);
+
+            if (image != null) {
+                int width = image.getWidth(null);
+                int height = image.getHeight(null);
+
+                for (int y = 0; y < height; y++) {
+                    for (int x = 0; x < width; x++) {
+                        int pixel = image.getRGB(x, y);
+                        Color c = new Color(pixel);
+                        int r = c.getRed();
+                        int g = c.getGreen();
+                        int b = c.getBlue();
+                        System.out.println(r + "," + g + "," + b);
+                    }
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
---
+++ lab2/src/main/java/ru/usernamedrew/tasks/Histogram.java
@@ -0,0 +1,4 @@
+package ru.usernamedrew.tasks;
+
+public class Histogram {
+}
